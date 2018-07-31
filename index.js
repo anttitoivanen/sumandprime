bb8a0d7ee15846336c051e6657386e78fb3d4e78
anttitoivanen <32294636+anttitoivanen@users.noreply.github.com>
2018-07-31 23:07:42 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const path = require("path")
+
+const app = express()
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+const checkPrime = (number) => {
+	const limit = Math.floor(Math.sqrt(number))
+	for (let divisor = 2; divisor <= limit; divisor++) {
+		if (number % divisor == 0) {
+			return false
+		}
+	}
+	return true
+}
+
+app.get('/primality/:num', function(req, res) {
+	const number = parseInt(req.params['num'])
+	if (isNaN(number) ||  number < 1) {
+		res.send({"error": "Non-integer or negative number found"})
+	}
+	res.send({"isPrime": checkPrime(number)})
+})
+
+app.get('/sumandprimality/:nums', function(req, res) {
+	const numbers = []
+	let number
+	for (let num of req.params['nums'].split(',')) {
+		number = parseInt(num)
+		if (isNaN(number) || number < 1) {
+			res.send({"error": "Non-integer or negtive number found"})
+		}
+		numbers.push(number)
+	}
+	const sum = numbers.reduce((a, b) => a + b)
+	const response = {
+		"sum": sum,
+		"isPrime": checkPrime(sum)
+	}
+	res.send(response)
+})
+
+app.listen(3000)
