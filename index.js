33b1d8b5e0346dbedd280425e404ef4d9a732ae4
Antti Toivanen <atoivane@gmail.com>
2018-07-31 23:43:43 +0300

    Improve handling when only checking for primes

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 const express = require('express')
-const path = require("path")
+const path = require('path')
 
 const app = express()
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'))
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 const checkPrime = (number) => {
-	const limit = Math.floor(Math.sqrt(number))
-	for (let divisor = 2; divisor <= limit; divisor++) {
-		if (number % divisor == 0) {
-			return false
-		}
-	}
-	return true
+  const limit = Math.floor(Math.sqrt(number))
+  for (let divisor = 2; divisor <= limit; divisor++) {
+    if (number % divisor === 0) {
+      return false
+    }
+  }
+  return true
 }
 
-app.get('/primality/:num', function(req, res) {
-	const number = parseInt(req.params['num'])
-	if (isNaN(number) ||  number < 1) {
-		res.send({"error": "Non-integer or negative number found"})
-	}
-	res.send({"isPrime": checkPrime(number)})
+app.get('/primality/:num', function (req, res) {
+  const number = parseInt(req.params['num'])
+  if (isNaN(number) || number < 1) {
+    res.send({'error': 'Non-integer or negative number found'})
+    return
+  }
+  res.send({'isPrime': checkPrime(number)})
 })
 
-app.get('/sumandprimality/:nums', function(req, res) {
-	const numbers = []
-	let number
-	for (let num of req.params['nums'].split(',')) {
-		number = parseInt(num)
-		if (isNaN(number) || number < 1) {
-			res.send({"error": "Non-integer or negtive number found"})
-		}
-		numbers.push(number)
-	}
-	const sum = numbers.reduce((a, b) => a + b)
-	const response = {
-		"sum": sum,
-		"isPrime": checkPrime(sum)
-	}
-	res.send(response)
+app.get('/sumandprimality/:nums', function (req, res) {
+  const numbers = []
+  let number
+  for (let num of req.params['nums'].split(',')) {
+    number = parseInt(num)
+    if (isNaN(number) || number < 1) {
+      res.send({'error': 'Non-integer or negtive number found'})
+      return
+    }
+    numbers.push(number)
+  }
+  const sum = numbers.reduce((a, b) => a + b)
+  const response = {
+    'sum': sum,
+    'isPrime': checkPrime(sum)
+  }
+  res.send(response)
 })
 
 app.listen(3000)
